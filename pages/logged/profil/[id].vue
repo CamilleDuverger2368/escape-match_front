fe1bf25e5d40ae550d3fee7ccc5f5b8df18a0031
grandma2368 <46317358+grandma2368@users.noreply.github.com>
2024-01-04 18:41:47 +0100

    feature : alter profil

---
+++ pages/logged/profil/[id].vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="alter-profil"></div>
+</template>
+
+<script setup>
+
+const route = useRoute()
+const token = useCookie("token")
+const runtimeConfig = useRuntimeConfig()
+
+onMounted(() => {
+    
+})
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/variables";
+
+#alter-profil {
+
+    width: 100%;
+    @include flex($direction:column);
+}
+</style>
