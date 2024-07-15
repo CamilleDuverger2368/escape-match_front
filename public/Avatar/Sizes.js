7cb8bf7385cf8957d1951e3453aaada88052c029
grandma2368 <46317358+grandma2368@users.noreply.github.com>
2024-07-15 14:56:28 +0200

    escapematch_front : fixed resize avatar

--- public/Avatar/Sizes.js
+++ public/Avatar/Sizes.js
@@ -15,19 +15,30 @@ export default class Sizes extends EventEmitter {
             } else {
                 this.width = window.innerWidth * 0.8
             }
-            this.height = 400
         } else {
 
             this.width = window.innerWidth
-            this.height = 400
         }
+        this.height = 400
         this.pixelRatio = Math.min(window.devicePixelRatio, 2)
 
         // listen Window resize event
         window.addEventListener("resize", () => {
 
-            this.width = window.innerWidth
-            this.height = window.innerHeight
+            if (page === "register") {
+
+                if (window.innerWidth > 1100) {
+                    this.width = window.innerWidth * 0.35
+                } else if (window.innerWidth > 450 && window.innerWidth <= 1100) {
+                    this.width = window.innerWidth * 0.6
+                } else {
+                    this.width = window.innerWidth * 0.8
+                }
+            } else {
+    
+                this.width = window.innerWidth
+            }
+            this.height = 400
             this.pixelRatio = Math.min(window.devicePixelRatio, 2)
 
             this.trigger("resize")
